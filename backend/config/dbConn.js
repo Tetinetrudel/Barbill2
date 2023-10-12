110ee0112527ecedee726fbb3c727e34efca82d0
Etienne Trudel <etrudel@gmail.com>
2023-10-12 12:12:25 -0400

    updated app

---
+++ backend/config/dbConn.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose'
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+    } catch (err) {
+        console.log(err)
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -16,16 +16,21 @@ import userRoutes from './routes/users.js'
 
 import { updateUser } from './controllers/users.js'
 import { corsOptions } from './config/corsOptions.js';
+import { connectDB } from './config/dbConn.js';
 
 /* variable settings */
 const app = express()
-//const PORT = process.env.PORT || 5000
-//const MONGODB_URI = process.env.MONGO_URI
+
+dotenv.config()
+
+const PORT = process.env.PORT || 3500
+
+connectDB()
 
 /* configuration settings */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config()
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors(corsOptions))
@@ -45,18 +50,6 @@ const upload = multer({ storage });
 /* ROUTES WITH FILES */
 app.patch("/users/:id", upload.single("picture"), updateUser);
 
-/* connection to the database */
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('Connecté à MongoDB')
-  })
-  .catch((error) => {
-    console.error('Erreur lors de la connexion à MongoDB:', error)
-})
-
 /* routes onfiguration */
 app.use('/auth', authRoutes)
 app.use('/clients', clientRoutes)
@@ -66,7 +59,8 @@ app.use('/invoices', invoiceRoutes)
 app.use('/users', userRoutes)
 
 /* connection to the app */
-app.listen(process.env.PORT, () => {
-    console.log(`Mongo DB est maintenant connecté et l'Application est lancé et fonctionne sur le port ${process.env.PORT}`)
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB')
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 })
 
