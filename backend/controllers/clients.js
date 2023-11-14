fa7f126225d12cd1fc4063a89172e5ac8c6cec25
Etienne Trudel <etrudel@gmail.com>
2023-11-13 20:47:29 -0500

    Updating clients search

--- backend/controllers/clients.js
+++ backend/controllers/clients.js
@@ -2,9 +2,9 @@ import Clients from '../models/clients.js'
 import Products from '../models/products.js'
 
 export const getClients = async (req, res) => {
-    const userId = req.userId
+    //const userId = req.userId
     try {
-        const clients = await Clients.find({ user: userId}).populate('products.product').populate('cards.product')
+        const clients = await Clients.find().populate('products.product').populate('cards.product')
         if(clients.length === 0) {
             return res.status(404).json({ message: `Aucun client trouvé`})
         }
